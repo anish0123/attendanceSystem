7c5f4845885f57b4b26957488b9497d01d19a4a7
riteshg <ritesh.ghimire@metropolia.fi>
2022-02-16 23:57:13 +0200

    added database codes on main activity for login and app crashes when the login() function is called

--- app/src/main/java/fi/metropolia/attendancesystem/MainActivity.java
+++ app/src/main/java/fi/metropolia/attendancesystem/MainActivity.java
@@ -3,12 +3,51 @@ package fi.metropolia.attendancesystem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.List;
+
+import fi.metropolia.attendancesystem.database.AppDataBase;
+import fi.metropolia.attendancesystem.database.Employee;
 
 public class MainActivity extends AppCompatActivity {
+    private AppDataBase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //login();
     }
+
+    public void login() {
+
+        //assigning id for edittext,errorTextView and login button
+        EditText employeeId = findViewById(R.id.userIdText);
+        EditText password = findViewById(R.id.passwordText);
+        TextView errorTextView = findViewById(R.id.errorText);
+        ImageButton signInBtn = findViewById(R.id.logInButton);
+
+        database = AppDataBase.getInstance(getApplicationContext());
+
+        Employee employee = new Employee("000", "Argie", "esimies", "argie123");
+
+
+        long id = database.employeeInterface().insert(employee);
+
+        //check existing esimies and avoid multiple entries
+        List<Employee> employeeList = database.employeeInterface().getAll();
+        for (Employee employee1 : employeeList) {
+            if (employee1.getColumnId() > 1 && employee1.getEmployeeRole().equals("esimies")) {
+                database.employeeInterface().delete(employee);
+            }
+
+        }
+
+    }
+
 }
