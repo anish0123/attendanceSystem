c7d0b14c2aa14d52cb6f0678f56ee2bf2b83f28d
anishm <anish.maharjan@metropolia.fi>
2022-02-17 11:55:01 +0200

    create new interface for database

--- app/src/main/java/fi/metropolia/attendancesystem/database/EmployeeDao.java
+++ app/src/main/java/fi/metropolia/attendancesystem/database/EmployeeDao.java
@@ -1,2 +1,34 @@
-package fi.metropolia.attendancesystem.database;public interface EmployeeDao {
+package fi.metropolia.attendancesystem.database;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface EmployeeDao {
+
+    @Insert
+    public Long insert (Employee employee);
+
+    @Update
+    public void update (Employee employee);
+
+    @Delete
+    public void delete (Employee employee);
+
+    @Query("SELECT * FROM employee ORDER BY columnId, employeeId")
+    public List <Employee> getAll();
+
+    @Query("SELECT * FROM employee WHERE employeeId like :employeeId")
+    public Employee getByEmployeeId (String employeeId);
+
+    @Query("SELECT * FROM employee WHERE name like :name")
+    public List <Employee>  getByName (String name);
+
+    @Query("SELECT * FROM employee WHERE role like :role")
+    public List <Employee> getByRole (String role);
 }
