826a5d38c4bc7fdaef081090786d4512053180f5
riteshg <ritesh.ghimire@metropolia.fi>
2022-02-28 10:28:28 +0200

    working on onBoarding

---
+++ app/src/main/java/fi/metropolia/attendancesystem/onBoardingLayout.java
@@ -0,0 +1,14 @@
+package fi.metropolia.attendancesystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class onBoardingLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding_layout);
+    }
+}
