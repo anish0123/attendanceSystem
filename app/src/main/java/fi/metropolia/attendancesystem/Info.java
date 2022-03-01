0bb3096459243aa3a30531b95e36bfcd1c61bb0a
Asmita Shrestha <asmita.shrestha@metropolia.fi>
2022-03-01 22:26:36 +0200

    modified info.java to make pop up window and fixed typos

--- app/src/main/java/fi/metropolia/attendancesystem/Info.java
+++ app/src/main/java/fi/metropolia/attendancesystem/Info.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
 public class Info extends AppCompatActivity {
 
@@ -12,8 +13,13 @@ public class Info extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
-        Intent intent = getIntent();
+        //making window as pop up window
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
 
+        int screenWidth = displayMetrics.widthPixels;
+        int screenHeight= displayMetrics.heightPixels;
 
+        getWindow().setLayout((int)(screenWidth),(int)(screenHeight*.7));
     }
 }
